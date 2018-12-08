30cec2ee3ac86e7c2b4cdbe6b9ba987c53a981a0
bartosz-paternoga <bpaternoga@hotmail.com>
2018-12-08 21:15:42 +0100

    3rd

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const moment = require('moment');
 moment().format();
