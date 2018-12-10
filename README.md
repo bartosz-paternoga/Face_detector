d797e543d0b43a2f3ce5d392e0ed7eae0c19d73b
bartosz-paternoga <bpaternoga@hotmail.com>
2018-12-10 11:32:43 +0100

    4th

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-This webapp is a face detector. Every new face (w. euclidean distance>0.4) is recorded, and a snapshot sent to ibm cloud storage. Information about date&time, as well as the link to the pic in cloud storage is recorded in database.  Face detector uses face-api.js, react ui and mongoDB.
+This webapp is a face detector. Every new face wth euclidean distance > 0.4 is recorded, 
+with a snapshot sent to ibm cloud storage. Information about date & time, as well as a
+link to the pic in cloud storage is recorded in database. Face detector uses face-api.js
+for ML algorithm, react UI for a frontend, node.js for a backend and mongoDB database.
+
+To start the face detector go to the 'Cam' tab and press 'Start predict', afterwards 'Stop
+predict'. The AI model needs to get loaded before you can begin. The logs with the links
+to the snapshots of new faces detected can be retrived from dsatabase (tab 'Data').
 
 Live demo:
 https://rocky-cove-67966.herokuapp.com
--- client/src/HomeComponent.js
+++ client/src/HomeComponent.js
@@ -24,7 +24,28 @@ const Home = () => {
 
     return (
         <div >
-           <br/> <pre id = "main">Face Detector</pre>
+           <br/> 
+           <pre id = "mainHome">
+            This webapp is a face detector. Every new face wth euclidean distance > 0.4 is recorded, </pre>
+           <br/> <pre id = "mainHome">
+           with a snapshot sent to ibm cloud storage. Information about date & time, as well as a 
+           </pre>
+           <br/> <pre id = "mainHome">
+           link to the pic in cloud storage is recorded in database. Face detector uses face-api.js  
+           </pre>
+           <br/><pre id = "mainHome">
+           for ML algorithm, react UI for a frontend, node.js for a backend and mongoDB database.
+           </pre>
+           <br/><pre id = "mainHome"></pre>
+            <br/><pre id = "mainHome">
+           To start the face detector go to the 'Cam' tab and press 'Start predict', afterwards 'Stop
+           </pre>
+           <br/><pre id = "mainHome">
+            predict'. The AI model needs to get loaded before you can begin. The logs with the links
+           </pre>
+           <br/><pre id = "mainHome">
+            to the snapshots of new faces detected can be retrived from dsatabase (tab 'Data'). 
+           </pre>
         </div>
     );
 }
